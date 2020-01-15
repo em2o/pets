748376257a242a1bfd06a8ab3d2e5d260dc687fc
Emanoel <emanuellimasantos2018sp@gmail.com>
2020-01-15 12:58:14 -0300

    aplicacao models

--- cachorro/models.py
+++ cachorro/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from dono.models import Dono
+
+class Cachorro(models.Model): 
+
+    nome = models.CharField(max_length=255)
+    idade = models.IntegerField()
+    porte = models.CharField(max_length=255)
+    cor_do_pelo = (
+        ('PRETO', 'Preto'),
+        ('BRANCO', 'Branco'),
+        ('PARDO', 'Pardo'),
+
+    )
+
+    cor = models.CharField(choices=cor_do_pelo, max_length=100)
+    dono = models.ManyToManyField(Dono, null=True)
 
 # Create your models here.
